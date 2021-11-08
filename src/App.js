e69833b25347d5db019de8ef02a6890d6bbdb427
Ebrahim Hadadi <hadadi.ebi@gmail.com>
2021-11-08 12:41:10 +0330

    react-jalali-datePicker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { DatePicker } from "jalali-react-datepicker";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DatePicker onClickSubmitButton={(value)=>{console.log(value)}}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
